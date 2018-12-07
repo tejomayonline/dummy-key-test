549c9f7b9e5cf5ed7acd214803e1b833b2f7c0ee
tejomayonline <tejomaysaha@outlook.com>
2018-12-07 12:41:34 +0530

    validate function bugfixed

---
+++ util.js
@@ -0,0 +1,15 @@
+const Joi =  require('joi');
+const courses = require('./mocks');
+
+function findCourseById(id) {
+    return courses.find(course => course.id === id);
+}
+
+function validateCourse(course) {
+    const schema = {
+        name : Joi.string().min(5).required()
+    };
+   return Joi.validate(course,schema);
+}
+
+module.exports = {findCourseById, validateCourse};
